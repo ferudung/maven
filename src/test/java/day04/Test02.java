5f58a04bb38e4bcdd0c149ca86c15d4eb474004c
ferudun gogus <ferudungogus2021@gmail.com>
2021-10-08 18:50:02 +0300

    v2

--- src/test/java/day04/Test02.java
+++ src/test/java/day04/Test02.java
@@ -3,6 +3,7 @@ package day04;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -25,11 +26,21 @@ public class Test02 {
         driver.findElement(By.xpath("//input[@name='submit']")).click();
         driver.get("http://zero.webappsecurity.com");
 //6. Pay Bills sayfasina gidin
-
-//7. amount kismina yatirmak istediginiz herhangi bir miktari yazin
-//8. tarih kismina “2020-09-10” yazdirin
-//9. Pay buttonuna tiklayin
-//10. “The payment was successfully submitted.” mesajinin ciktigini control edin
+        driver.findElement(By.xpath("//strong[text()='Online Banking']")).click();
+        driver.findElement(By.id("pay_bills_link")).click();
+        //7. amount kismina yatirmak istediginiz herhangi bir miktari yazin
+        driver.findElement(By.id("sp_amount")).sendKeys("1000");
+        //8. tarih kismina “2020-09-10” yazdirin
+        driver.findElement(By.id("sp_date")).sendKeys("2020-09-10");
+        //9. Pay buttonuna tiklayin
+        driver.findElement(By.id("pay_saved_payees")).click();
+        //10. “The payment was successfully submitted.” mesajinin ciktigini control edin
+        WebElement basariliYazisielementi=driver.findElement(By.id("alert_content"));
+        if (basariliYazisielementi.isDisplayed()){
+            System.out.println("Test PASS");
+        }else {
+            System.out.println("test FAILED");
+        }
 
 
 
--- src/test/java/day04/Test03.java
+++ src/test/java/day04/Test03.java
@@ -1,16 +1,39 @@
 package day04;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class Test03 {
     public static void main(String[] args) {
 //        1. “https://www.saucedemo.com” Adresine gidin
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://www.saucedemo.com");
 //        2. Username kutusuna “standard_user” yazdirin
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
 //        3. Password kutusuna “secret_sauce” yazdirin
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
 //        4. Login tusuna basin
+        driver.findElement(By.id("login-button")).click();
 //        5. Ilk urunun ismini kaydedin ve bu urunun sayfasina gidin
+        WebElement ilkIsım=driver.findElement(By.xpath("//a[@id='item_4_title_link']"));
+        System.out.println(ilkIsım.getText());
 //        6. Add to Cart butonuna basin
+        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
 //        7. Alisveris sepetine tiklayin
+        driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
 //        8. Sectiginiz urunun basarili olarak sepete eklendigini control edin
+        WebElement urunKontrol=driver.findElement(By.xpath("//a[@id='item_4_title_link']"));
+        System.out.println(urunKontrol.getText());
 //        9. Sayfayi kapatin
+        driver.close();
 
     }
 }
